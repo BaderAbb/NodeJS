4a565acddae1ee8afb1641bdcca24681f1afa532
bader <babbaoui01@gmail.com>
2026-01-19 09:43:36 +0100

    more express test

--- Express/server.js
+++ Express/server.js
@@ -3,11 +3,16 @@ const satoruJSON = require('./JJK/satoru.json')
 const app = express()
 const port = 3000
 
+const data = ['Satoru']
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
   console.log('yay, I hit an endpoint', req.method)
-  res.send('<h1>Hello world!!</h1>')
+  res.send(`<body>
+            <h1>Data:<h1>
+            <p>${JSON.stringify(data)}</p>
+            </body>`)
 })
 
 app.get('/test', (req, res) => {
@@ -21,8 +26,12 @@ app.get('/gojo', (req, res) => {
 
 app.post('/data', (req, res) => {
   const newEntry = req.body
-  res.sendStatus(201)
   console.log(newEntry)
+  data.push(newEntry.name)
+  console.log(data)
+  res.sendStatus(201)
 })
+
+app.delete()
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
